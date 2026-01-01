0c3601eb5044834ad4e6efcd92f39904bc00f5eb
Aditya Arya <64355995+AdArya125@users.noreply.github.com>
2026-01-01 23:22:11 +0530

    added implementation of sum, mul, div

---
+++ P3-Bytecode-Virtual-Machine/src/vm.c
@@ -0,0 +1,136 @@
+//vm-> running = 0;  it is done in stack overflow, stackunderflow, unknown opcode, HALT instructions
+#include <stdio.h>
+#include <stdlib.h>
+#include "vm.h"
+#include "instruction.h"
+
+#define STACK_MAX 1024 //for now keeping stack capacity as 1024
+
+//adding this part to show execution over stack
+static void vm_dump_stack(VM *vm) 
+{
+    printf("STACK [size=%u]: ", vm->sp);
+    for (uint32_t i = 0; i < vm->sp; i++) {
+        printf("%d ", vm->stack[i]);
+    }
+    printf("\n");
+}
+
+//basic push pop is done here with proper bound check
+static void vm_push(VM *vm, int32_t value) 
+{
+    if (vm->sp >= vm->stack_capacity) 
+    {
+        fprintf(stderr, "Runtime error: stack overflow\n");
+        vm->running = 0;
+        return;
+    }
+    vm->stack[vm->sp++] = value; //stack pointer increment happens here
+}
+
+static int32_t vm_pop(VM *vm) 
+{
+    if (vm->sp == 0) 
+    {
+        fprintf(stderr, "Runtime error: stack underflow\n");
+        vm->running = 0;
+        return 0;
+    }
+    return vm->stack[--vm->sp];
+}
+
+//initialise VM here
+void vm_init(VM *vm, uint8_t *code, size_t code_size) 
+{
+    vm->code = code;
+    vm->code_size = code_size;
+    vm->pc = 0;
+
+    vm->stack_capacity = STACK_MAX;
+    vm->stack = malloc(sizeof(int32_t) * STACK_MAX);  //allocating stack to vm here
+    vm->sp = 0;
+    vm->fp = 0;
+
+    vm->running = 1;
+}
+
+void vm_run(VM *vm) 
+{
+    while (vm->running) 
+    {
+        if (vm->pc >= vm->code_size) 
+        {
+            fprintf(stderr, "Runtime error: PC out of bounds\n");
+            break;
+        }
+
+        uint8_t opcode = vm->code[vm->pc++]; // this is fetch , reads instruction and increment PC
+        
+        //decode happens below , in modern CPU this happens via microcode and jump tables
+        switch (opcode) 
+        {
+        //OPpush works as an execute unit, reads operand, opdate PC and modify stack
+        //we have implemented here immidiate addressing as well as Variable length instructions
+        case OP_PUSH: 
+            // operand is next 4 bytes (int32)
+            int32_t value = *(int32_t *)&vm->code[vm->pc];
+            vm->pc += 4;
+            vm_push(vm, value);
+            break;
+        
+        //--------------Arithmatic section here ----------------------
+        // stack will be LIFO, order is done to mirror ALU semantics a+b and b+a is different for stack
+        case OP_ADD: {
+            int32_t b = vm_pop(vm);
+            int32_t a = vm_pop(vm);
+            vm_push(vm, a + b);
+            break;
+        }
+
+        case OP_SUB: {
+            int32_t b = vm_pop(vm);
+            int32_t a = vm_pop(vm);
+            vm_push(vm, a - b);
+            break;
+        }
+
+        case OP_MUL: {
+            int32_t b = vm_pop(vm);
+            int32_t a = vm_pop(vm);
+            vm_push(vm, a * b);
+            break;
+        }
+
+        case OP_DIV: {
+            int32_t b = vm_pop(vm);
+            int32_t a = vm_pop(vm);
+        
+            if (b == 0) {
+                fprintf(stderr, "Runtime error: division by zero\n");
+                vm->running = 0;
+                break;
+            }
+        
+            vm_push(vm, a / b);
+            break;
+        }
+
+        //____________________________________________________________
+        //simple stop done to avoid segfault and undefined behavious
+        case OP_HALT:
+            vm->running = 0; 
+            break;
+
+        default:
+            fprintf(stderr, "Unknown opcode: 0x%02X\n", opcode);
+            vm->running = 0;
+        }
+        vm_dump_stack(vm); //added to show execution in stack what happens and how, for later part will guard with a flag
+    }
+}
+
+//freeing VM here
+void vm_free(VM *vm) 
+{
+    free(vm->stack);
+}
